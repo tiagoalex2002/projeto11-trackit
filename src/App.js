5cb2c5fc8bfcdfdbcae814bfed5d5c56891864b6
Tiago <tiagoalex0202@gmail.com>
2023-03-14 16:29:15 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useState } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App() {
+    return (
+        <BrowserRouter>
+           <NavContainer>CINEFLEX</NavContainer>
+            <Routes>
+               <Route path="/" element={<Login/>}/>
+               <Route path="/cadastro" element={<Cadastro/>} />
+               <Route path="/historico" element={<Histórico/>}/>
+               <Route path="/hoje" element={<Hoje />}/>
+               <Route path="/habitos" elemento={<Hábitos/>}/>
+            </Routes>
+            
+        </BrowserRouter>
+    )
 }
 
-export default App;
+const NavContainer = styled.div`
+    width: 100%;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #C3CFD9;
+    color: #E8833A;
+    font-family: 'Roboto', sans-serif;
+    font-size: 34px;
+    position: fixed;
+    top: 0;
+    a {
+        text-decoration: none;
+        color: #E8833A;
+    }
+`
