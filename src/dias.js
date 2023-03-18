7541bc9584ed64b150d5cc826dddf39c106dfa7a
Tiago <tiagoalex0202@gmail.com>
2023-03-18 17:45:23 -0300

    correção do número do domingo

--- src/dias.js
+++ src/dias.js
@@ -1,3 +1,3 @@
-const dias=[{dia:"S", number:1},{dia:"T", number:2},{dia:"Q", number:3},{dia:"Q",number:4},{dia:"S",number:5},{dia:"S",number:6},{dia:"D",number:7}]
+const dias=[{dia:"D",number:0},{dia:"S", number:1},{dia:"T", number:2},{dia:"Q", number:3},{dia:"Q",number:4},{dia:"S",number:5},{dia:"S",number:6}]
 
 export default dias
